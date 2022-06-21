df1746cdbb9649b94f1ec29fd988ecfed117d568
Steven Calderon <scalderon1504@gmail.com>
2022-06-20 22:16:14 -0500

    Se modifica el comando abrir orden de taler

--- domain/src/main/java/co/com/sofka/reto/ordentaller/commands/AbrirOrdenDeTaller.java
+++ domain/src/main/java/co/com/sofka/reto/ordentaller/commands/AbrirOrdenDeTaller.java
@@ -4,23 +4,17 @@ import co.com.sofka.domain.generic.Command;
 import co.com.sofka.reto.cliente.identities.ClienteId;
 import co.com.sofka.reto.ordentaller.OperacionFactory;
 import co.com.sofka.reto.ordentaller.TecnicoFactory;
-import co.com.sofka.reto.ordentaller.identities.OrdenTallerId;
 
 public class AbrirOrdenDeTaller extends Command {
-    private final OrdenTallerId ordenTallerId;
     private final TecnicoFactory tecnicoFactory;
     private final OperacionFactory operacionFactory;
     private final ClienteId clienteId;
 
-    public AbrirOrdenDeTaller(OrdenTallerId ordenTallerId, TecnicoFactory tecnicoFactory, OperacionFactory operacionFactory, ClienteId clienteId) {
-        this.ordenTallerId = ordenTallerId;
+    public AbrirOrdenDeTaller(TecnicoFactory tecnicoFactory, OperacionFactory operacionFactory, ClienteId clienteId) {
+
         this.tecnicoFactory = tecnicoFactory;
         this.operacionFactory = operacionFactory;
         this.clienteId = clienteId;
-    }
-
-    public OrdenTallerId getOrdenTallerId() {
-        return ordenTallerId;
     }
 
     public TecnicoFactory getTecnicoFactory() {
