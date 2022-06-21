7ef9b3a1d3912207d56d61ddae70477f84f67b59
Steven Calderon <scalderon1504@gmail.com>
2022-06-20 22:15:38 -0500

    Correccion value object name

--- domain/src/main/java/co/com/sofka/reto/generic/values/Nombre.java
+++ domain/src/main/java/co/com/sofka/reto/generic/values/Nombre.java
@@ -1,4 +1,40 @@
 package co.com.sofka.reto.generic.values;
 
-public class Nombre {
+import co.com.sofka.domain.generic.ValueObject;
+
+import java.util.Objects;
+
+public class Nombre implements ValueObject<String> {
+    private final String nombre;
+
+    public Nombre(String nombre) {
+        this.nombre = Objects.requireNonNull(nombre);
+        if (this.nombre.isBlank()) {
+            throw new IllegalArgumentException("Se requiere un nombre");
+        }
+        if (this.nombre.length() < 3) {
+            throw new IllegalArgumentException("El nombre es muy corto");
+        }
+        if (!this.nombre.matches("^[a-zA-ZÀ-ÿ\\u00f1\\u00d1_]+(\\\\s+[a-zA-ZÀ-ÿ\\u00f1\\u00d1_]+)*$")) {
+            throw new IllegalArgumentException("Ingresa un nombre valido");
+        }
+    }
+
+    @Override
+    public String value() {
+        return nombre;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Nombre nombre1 = (Nombre) o;
+        return nombre.equalsIgnoreCase(nombre1.nombre);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nombre);
+    }
 }
