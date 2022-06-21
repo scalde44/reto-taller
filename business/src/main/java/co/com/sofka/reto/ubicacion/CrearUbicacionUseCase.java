5c24b3eddbde2f75d63d2333ef33f245ee455cc5
Steven Calderon <scalderon1504@gmail.com>
2022-06-20 22:17:31 -0500

    Creacion de casos de uso para ubicacion

---
+++ business/src/main/java/co/com/sofka/reto/ubicacion/CrearUbicacionUseCase.java
@@ -0,0 +1,16 @@
+package co.com.sofka.reto.ubicacion;
+
+import co.com.sofka.business.generic.UseCase;
+import co.com.sofka.business.support.RequestCommand;
+import co.com.sofka.business.support.ResponseEvents;
+import co.com.sofka.reto.ubicacion.commands.CrearUbicacion;
+import co.com.sofka.reto.ubicacion.identities.UbicacionId;
+
+public class CrearUbicacionUseCase extends UseCase<RequestCommand<CrearUbicacion>, ResponseEvents> {
+    @Override
+    public void executeUseCase(RequestCommand<CrearUbicacion> input) {
+        var command = input.getCommand();
+        var ubicacion = new Ubicacion(new UbicacionId(), command.getEspacioId(), command.getEstado(), command.getDireccion(), command.getCategoria());
+        emit().onResponse(new ResponseEvents(ubicacion.getUncommittedChanges()));
+    }
+}
